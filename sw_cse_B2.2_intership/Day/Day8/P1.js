15da37146b25575963021d35146199b000d14395
sheshadri-21 <tanujatadakod@gmail.com>
2026-08-13 18:19:05 +0530

    java script usage

--- sw_cse_B2.2_intership/Day/Day8/P1.js
+++ sw_cse_B2.2_intership/Day/Day8/P1.js
@@ -1,4 +1,7 @@
-// JS Variables
-let name = "sheshadri";
-console.log(name) 
-console.log(5 === "5");
+document.addEventListener("DOMContentLoaded", function () {
+  const myButton = document.getElementById("external-js");
+
+  myButton.addEventListener("click", function () {
+    alert("Hello Learners, Good morning. This is External JS");
+  });
+});
